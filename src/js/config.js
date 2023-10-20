f194ecbe78e3be59d44ba9607311dad853f3e307
MhL5 <sapumr@gmail.com>
2023-10-20 13:19:05 +0330

    api key update

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '63b8291b-2c36-42ae-9d0a-77e9211b8942';
 export const MODAL_CLOSE_SEC = 2.5;
